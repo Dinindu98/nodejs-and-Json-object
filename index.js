29c2ae2cb782fd0e86381b5f8a98aca06c184993
Dinindu98 <dinindu.ayagama@gmail.com>
2019-11-01 14:40:06 -0700

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var fs = require('fs');
-var words = fs.readFileSync('abc.json');
-var data = JSON.parse(words);
-console.log(data[0].name);
+
+
 
 var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('./public'));
+
 app.get('/',function(req,res){
     // res.send('this i' + data[1].name + data[1].artist);
+    var words = fs.readFileSync('abc.json');
+    var data = JSON.parse(words);   
     res.render('home',{data:data});
+    console.log(data[0].name);
+    console.log(data[0].artist);
     
 });
 
